5cd50c6af22c336898021eb68ee3c26427308195
Karim <karim.almas@nu.edu.kz>
2020-07-29 21:59:13 +0600

    Initial Commit

---
+++ CACTUS2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CACTUS2
+//
+//  Created by карим on 7/29/20.
+//  Copyright © 2020 Karim Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
